f95cb1f571502d354d6c1a02e3e2f3366f65411e
JonathanAndrade19 <jonathanjampamb@gmail.com>
2022-12-05 20:56:32 -0300

    criando o componente de listar pensamentos

---
+++ src/app/components/pensamentos/lista-pensamentos/lista-pensamentos.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListaPensamentosComponent } from './lista-pensamentos.component';
+
+describe('ListaPensamentosComponent', () => {
+  let component: ListaPensamentosComponent;
+  let fixture: ComponentFixture<ListaPensamentosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListaPensamentosComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListaPensamentosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/pensamentos/lista-pensamentos/lista-pensamentos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-pensamentos',
+  templateUrl: './lista-pensamentos.component.html',
+  styleUrls: ['./lista-pensamentos.component.css']
+})
+export class ListaPensamentosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
