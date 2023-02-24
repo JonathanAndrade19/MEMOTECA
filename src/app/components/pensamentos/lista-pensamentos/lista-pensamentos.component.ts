2aeddbd3f85d702cb4edafd9074289d707d34101
JonathanAndrade19 <jonathanjampamb@gmail.com>
2023-02-24 14:54:56 -0300

    finalizando projeto memoteca

--- src/app/components/pensamentos/lista-pensamentos/lista-pensamentos.component.ts
+++ src/app/components/pensamentos/lista-pensamentos/lista-pensamentos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Pensamento } from 'src/app/core/pensamento';
 import { PensamentoService } from 'src/app/service/pensamento.service';
 
@@ -13,17 +14,21 @@ export class ListaPensamentosComponent implements OnInit {
   paginaAtual: number = 1;
   haMaisPensamentos: boolean = true;
   filtro: string = '';
+  favoritos: boolean = false;
+  listaFavoritos: Pensamento[] = [];
+  titulo: string = 'Meu Mural';
 
-  constructor(private service: PensamentoService) { }
+  constructor(private service: PensamentoService, private router: Router) { }
 
   ngOnInit(): void {
-    this.service.listarPensamentos(this.paginaAtual, this.filtro).subscribe((dados) => {
+    this.service.listarPensamentos(this.paginaAtual, this.filtro, this.favoritos).subscribe(
+      (dados) => {
       this.listaPensamentos = dados;
     })
   }
 
   carregarMaisPensamentos(){
-    this.service.listarPensamentos(++this.paginaAtual, this.filtro).subscribe(
+    this.service.listarPensamentos(++this.paginaAtual, this.filtro, this.favoritos).subscribe(
       (dados) => {
         this.listaPensamentos.push(...dados);
         if(!dados.length){
@@ -36,11 +41,31 @@ export class ListaPensamentosComponent implements OnInit {
   pesquisarPensamentos(){
     this.haMaisPensamentos = true;
     this.paginaAtual = 1;
-    this.service.listarPensamentos(this.paginaAtual, this.filtro).subscribe(
+    this.service.listarPensamentos(this.paginaAtual, this.filtro, this.favoritos).subscribe(
       (dados) => {
         this.listaPensamentos = dados;
       }
     )
+  }
+
+  listarFavoritos(){
+    this.titulo = 'Meus Favoritos';
+    this.haMaisPensamentos = true;
+    this.favoritos = true;
+    this.paginaAtual = 1;
+    this.service.listarPensamentos(this.paginaAtual, this.filtro, this.favoritos).subscribe(
+      (dados) => {
+      this.listaPensamentos = dados;
+      this.listaFavoritos = dados;
+    })
+  }
+
+  recarregarComponente(){
+    this.favoritos = false;
+    this.paginaAtual = 1;
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload'
+    this.router.navigate([this.router.url])
   }
 
 }
--- src/app/service/pensamento.service.ts
+++ src/app/service/pensamento.service.ts
@@ -12,12 +12,15 @@ export class PensamentoService {
 
   constructor(private http: HttpClient) { }
 
-  listarPensamentos(pagina: number, filtro: string): Observable<Pensamento[]>{
+  listarPensamentos(pagina: number, filtro: string, favoritos: boolean): Observable<Pensamento[]>{
     const itensPorPagina = 6;
     
     let params = new HttpParams().set("_page", pagina).set("_limit", itensPorPagina)
     if (filtro.trim().length > 2){
       params = params.set("q", filtro)
+    }
+    if(favoritos){
+      params = params.set("favorito", true)
     }
 
     return this.http.get<Pensamento[]>(this.API, {params: params})
